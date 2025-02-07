a4dd5955447d60fa1ff3cec2eb9aee030b734885
NeverMindHarley <harley.gutierrezcarbonell@gmail.com>
2025-02-07 11:13:45 +0800

    components public

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -5,13 +5,16 @@ import React from 'react'
 import { buttonVariants } from './ui/button'
 import HackerBtn from './animation/HackerBtn'
 
-function DownLoadResumeBtn() {
+const DownLoadResumeBtn = () => {
   return (
-    <div className="h-fit w-full mt-2 py-2 px-4">
-  
-    <HackerBtn label='Download Resume' />
-  </div>
-  )
-}
+    <div className="mt-4">
+      <Link href="/Updated Resume Carbonell.pdf " download>
+        <a className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+          Download Resume
+        </a>
+      </Link>
+    </div>
+  );
+};
 
-export default DownLoadResumeBtn
+export default DownLoadResumeBtn;
